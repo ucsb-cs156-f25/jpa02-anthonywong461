030feb3696646776b6dc3c76e261f353e79466f6
Anthony Wong <anthonywong@100-64-55-240.wireless.ucsb.edu>
2025-10-07 17:37:53 -0700

    test

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -16,11 +16,7 @@ public class Developer {
      */
 
     public static String getName() {
-        // TODO: Change this to your name
-        // You may use just the name that is used on <https://bit.ly/cs156-f25-teams>
-        // i.e. your first name, or your first and initial of last name
-
-        return "Chris G.";
+        return "Anthony W.";
     }
 
     /**
@@ -29,8 +25,7 @@ public class Developer {
      */
 
     public static String getGithubId() {
-        // TODO: Change this to your github id
-        return "cgaucho";
+        return "anthonywong461";
     }
 
     /**
@@ -40,13 +35,13 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("f25-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("f25-04");
+        team.addMember("Ajay P.");
+        team.addMember("Chiran A.");
+        team.addMember("Anthony W.");
+        team.addMember("Dave B.");
+        team.addMember("Max C.");
+        team.addMember("Ryan H.");
         return team;
     }
 }
