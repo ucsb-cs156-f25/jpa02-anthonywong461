1d4ff8f8d537a6ebf23f45dd5ac5ee60079efe33
Anthony Wong <anthonywong@100-64-55-240.wireless.ucsb.edu>
2025-10-08 16:42:10 -0700

    hi

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -22,5 +22,72 @@ public class TeamTest {
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    //true
+    @Test
+    public void equals_returnsTrueForSameNameAndMembers() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("bar");
+        t1.addMember("bar2");
+
+        Team t2 = new Team();
+        t2.setName("foo");
+        t2.addMember("bar");
+        t2.addMember("bar2");
+        assertEquals(true, t1.equals(t2));
+        assertEquals(t1.hashCode(), t2.hashCode()); // hashCode consistent
+    }
+
+    @Test
+    public void equals_Test() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("bar");
+        assertEquals(t1.equals(t1), true);
+    }
+
+    @Test
+    public void equals_Test3() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("foo");
+        Team t2 = new Team();
+        t2.setName("foo");
+        t2.addMember("bar");
+        assertEquals(t1.equals(t2), false);
+    }
+
+    @Test
+    public void equals_Test2() {
+        Team t1 = new Team("foo");
+        t1.addMember("bar");
+
+        Team t2 = new Team("bar");
+        t2.addMember("foo");
+
+
+        assertEquals(t1.equals(t2),false);
+    }
+
+@Test
+public void equals_returnsFalseForNull() {
+    Team t1 = new Team("foo");
+    assertEquals(t1.equals(null),false);  // null is not a Team
+}
+
+    @Test
+    public void hashCode_returnsExpectedValue() {
+        Team t = new Team();
+        t.setName("foo");
+        t.addMember("bar");
+        int expected = t.name.hashCode() | t.getMembers().hashCode();
+        assertEquals(expected, t.hashCode());
+    }
+
 
 }
